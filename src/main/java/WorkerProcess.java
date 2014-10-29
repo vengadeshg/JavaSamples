994ce19b7f2949f0b329ef7e6b6a566c766be5db
CVHS2 <venkatesh2411@gmail.com>
2014-10-29 17:30:38 +0530

    ergre
    regr

--- src/main/java/WorkerProcess.java
+++ src/main/java/WorkerProcess.java
@@ -5,13 +5,6 @@ public class WorkerProcess
 {
     public static void main(String[] args)
     {
-        while(true) {
-            try {
-                Thread.sleep(1000);
-            } catch(InterruptedException e) {}
-
-            System.out.println("Worker process woke up");
-
-        }
+            System.out.println("My FIrst Java Application");
     }
 }
