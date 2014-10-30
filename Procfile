bc246fa031763974c34deaa3d627a67557714f52
CVHS2 <venkatesh2411@gmail.com>
2014-10-30 11:03:43 +0530

    Committed
    Committed

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
-web: java $JAVA_OPTS -cp target/classes:target/dependency/* Main
+web:    java -cp target/classes:target/dependency/* embeddedTomcatSample
 
 
--- src/main/java/WorkerProcess.java
+++ src/main/java/WorkerProcess.java
@@ -1,10 +1,7 @@
-
-
-
 public class WorkerProcess
 {
     public static void main(String[] args)
     {
-            System.out.println("My FIrst Java Application");
+            System.out.println("My First Java Application");
     }
 }
